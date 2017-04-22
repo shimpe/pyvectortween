d19a36fda0bfc9d221d65bde1612ff6181fca66d
stefaan himpe <stefaan.himpe@gmail.com>
2017-04-23 00:24:18 +0200

    add proposed setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='vectortween',
+    version='0.0.1',
+    packages=['vectortween'],
+    url='',
+    license='MIT',
+    author='stefaan himpe',
+    author_email='stefaan.himpe@gmail.com',
+    description='some tweening for use with libraries like gizeh and moviepy'
+)
