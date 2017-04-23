4e8174892062c1366545c250e7375796857b8d1b
stefaan himpe <stefaan.himpe@gmail.com>
2017-04-23 08:50:15 +0200

    add some mappings

--- vectortween/Mapping.py
+++ vectortween/Mapping.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Mapping(object):
     """
     class to map numbers from one range to another range
@@ -7,14 +10,93 @@ class Mapping(object):
         pass
 
     @staticmethod
-    def linlin(value, a, b, c, d):
+    def linlin(value, in_min, in_max, out_min, out_max):
         """
-            maps value \in [a,b] linearly to the corresponding value \in [c, d]
-            e.g. linlin(0.3,0,1,10,20) = 13
+        maps value \in [in_min,in_max] linearly to the corresponding value \in [out_min, out_max]
+        (extrapolating if needed)
+        e.g. linlin(0.3,0,1,10,20) = 13
+            
+        :param value: value to be mapped 
+        :param in_min: input range minimum
+        :param in_max: input range maximum
+        :param out_min: what input range minimum is mapped to
+        :param out_max: what input range maximum is mapped to
+        :return: linear mapping from value in input range to value in output range (extrapolating if needed)
+         
+         example: linlin(0.2, 0, 1, 10, 20) = 13
         """
-        if a == b:
-            if value == a and c == d:
-                return c
+        if in_min == in_max:
+            if value == in_min and out_min == out_max:
+                return out_min
             return None
 
-        return ((c + d) + (d - c) * ((2 * value - (a + b)) / float(b - a))) / 2.0
+        return ((out_min + out_max) + (out_max - out_min) * (
+                (2 * value - (in_min + in_max)) / float(in_max - in_min))) / 2.0
+
+    @staticmethod
+    def linexp(value, in_min, in_max, out_min, out_max):
+        """
+        maps value \in linear range [in_min,in_max] corresponding value \in exponential range [out_min, out_max]
+        (extrapolating if needed)
+
+        :param value: value to be mapped 
+        :param in_min: input range minimum
+        :param in_max: input range maximum
+        :param out_min: what input range minimum is mapped to
+        :param out_max: what input range maximum is mapped to
+        :return: mapping from value in linear input range to value in exponential output range (extrapolating if needed)
+        """
+        if in_min == in_max:
+            if value == in_min and out_min == out_max:
+                return out_min
+            return None
+
+        return math.pow(out_max / out_min, (value - in_min) / (in_max - in_min)) * out_min
+
+    @staticmethod
+    def explin(value, in_min, in_max, out_min, out_max):
+        """
+        maps value \in exponential range [in_min,in_max] corresponding value \in linear range [out_min, out_max]
+        (extrapolating if possible)
+
+        :param value: value to be mapped 
+        :param in_min: input range minimum
+        :param in_max: input range maximum
+        :param out_min: what input range minimum is mapped to
+        :param out_max: what input range maximum is mapped to
+        :return: mapping from value in  exponential input range to value in linear output range 
+        (extrapolating if possible)
+        """
+        if in_min == 0:
+            return None
+        if (value / in_min) < 0:
+            return None
+        if (in_max / in_min) < 0:
+            return None
+
+        return math.log(value / in_min) / math.log(in_max / in_min) * (out_max - out_min) + out_min
+
+    @staticmethod
+    def expexp(value, in_min, in_max, out_min, out_max):
+        """
+        maps value \in exponential range [in_min,in_max] corresponding value \in exponential range [out_min, out_max]
+        (extrapolating if possible)
+
+        :param value: value to be mapped 
+        :param in_min: input range minimum
+        :param in_max: input range maximum
+        :param out_min: what input range minimum is mapped to
+        :param out_max: what input range maximum is mapped to
+        :return: mapping from value in  exponential input range to value in exponential output range 
+        (extrapolating if possible)
+        """
+        if value == 0:
+            return None
+        if value / in_min < 0:
+            return None
+        if in_min == 0:
+            return None
+        if (in_max - in_min) < 0:
+            return None
+
+        return math.pow(out_max / out_min, math.log(value / in_min) / math.log(in_max / in_min)) * out_min
