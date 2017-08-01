b53a68f845060b34de67755d876c9b2c587e1d26
stefaan himpe <stefaan.himpe@gmail.com>
2017-08-01 17:39:24 +0200

    update to latest setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
-import sys
 from distutils.core import setup
-
-if sys.version_info[0] < 3:
-    sys.exit("Sorry. Python 2 is currently not supported.")
+from setuptools import find_packages
 
 setup(
     name='vectortween',
-    version='0.0.1',
-    packages=['vectortween'],
+    version='0.0.2',
+    packages=find_packages(exclude=['build', 'dist', 'docs', 'tests']),
+    python_requires='>=3',
     url='https://github.com/shimpe/pyvectortween',
+    keywords=["tween", "animation"],
     license='MIT',
     author='stefaan himpe',
     author_email='stefaan.himpe@gmail.com',
-    description='vector animation library with tweening',
-    install_requires=['pytweening', 'sympy', 'numpy', 'scipy']
+    description='animation library with tweening',
+    long_description='animation library with tweening',
+    install_requires=['pytweening', 'sympy', 'numpy', 'scipy'],
+    classifiers=["Development Status :: 4 - Beta",
+                "Intended Audience :: Developers",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",
+                "Programming Language :: Python :: 3 :: Only",
+                "Topic :: Artistic Software",
+                "Topic :: Software Development :: Libraries",
+                "Topic :: Software Development :: Libraries :: Python Modules"]
 )
